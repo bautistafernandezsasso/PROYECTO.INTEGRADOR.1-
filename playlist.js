abd6aad2e6bf2121756ec0fa54aabbb6f3486353
SantiBp21 <sblancopan@udesa.edu.ar>
2021-06-22 14:44:49 -0300

    widget deezer

--- playlist.js
+++ playlist.js
@@ -24,12 +24,14 @@ fetch (apiPlaylist)
     nombreAr = document.querySelector(".nombreartista");
     nombreCancion = document.querySelector(".nombrecancion");
     nombreDisco = document.querySelector(".nombredisco");
+    musicPlayer = document.querySelector(".musicplayer")
     fotoAr.innerHTML +=`
     <img src="${datos.tracks.data[1].album.cover_medium}">`;
     
     nombreCancion.innerHTML +=`<h2>${datos.tracks.data[1].title}</h2>`;
     nombreAr.innerHTML +=`<h2>${datos.tracks.data[1].artist.name}</h2>`;
     nombreDisco.innerHTML += `<h2>${datos.tracks.data[1].album.title}</h2>`;
+    musicPlayer.innerHTML += `<iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/track/${datos.tracks.data[1].id}" width="410" height="500" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>`;
 
 
 
