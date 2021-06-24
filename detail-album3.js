2045c7b840de6ac7f36268807d64ed2beeefe22b
bautistafernandezsasso <bautifernandezs@gmail.com>
2021-06-24 01:00:11 -0300

    details albums

---
+++ detail-album3.js
@@ -0,0 +1,57 @@
+window.addEventListener('load',function(){
+
+    /*Funcion Para menu desplegable responsive*/
+    const navToggle = document.querySelector(".nav-toggle")
+    const navMenu =document.querySelector(".nav-menu")
+    
+    navToggle.addEventListener("click",() =>{
+        navMenu.classList.toggle("nav-menu_visible");
+    })
+    
+
+   const proxy = `https://cors-anywhere.herokuapp.com/`;
+
+let apiAlbums = `${proxy}https://api.deezer.com/album/217281`;
+
+fetch (apiAlbums)                                           
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+    console.log(datos)
+   
+    fotoAlb3= document.querySelector(".fotoartist3");
+    nombreAlb3 = document.querySelector(".nombrealbum3");
+    nombreArt3 = document.querySelector(".nombreart3");
+    nombreGen3 = document.querySelector(".nombregen3");
+    fechaAlb3 = document.querySelector(".fechaalb3");
+    listaTemas3 = document.querySelector(".temasalbum3")
+
+  fotoAlb3.innerHTML +=`
+    <img src="${datos.cover_medium}">`;
+    
+    nombreAlb3.innerHTML +=`<h2>${datos.title}</h2>`;
+    nombreArt3.innerHTML +=`<h2>${datos.artist.name}</h2>`;
+    nombreGen3.innerHTML += `<h2>${datos.genres.data[0].name}</h2>`;
+    fechaAlb3.innerHTML += `<h2>${datos.release_date}</h2>`;
+    listaTemas3.innerHTML += `<li>${datos.tracks.data[0].title}</li>
+    <li>${datos.tracks.data[1].title}</li>
+    <li>${datos.tracks.data[2].title}</li>
+    <li>${datos.tracks.data[3].title}</li>
+    <li>${datos.tracks.data[4].title}</li>
+    <li>${datos.tracks.data[5].title}</li>
+    <li>${datos.tracks.data[6].title}</li>
+    <li>${datos.tracks.data[7].title}</li>
+    <li>${datos.tracks.data[8].title}</li>
+    <li>${datos.tracks.data[9].title}</li>
+    <li>${datos.tracks.data[10].title}</li>
+    <li>${datos.tracks.data[11].title}</li>`;
+    
+    
+
+    })
+    .catch(function(error){
+console.log(error)
+    })
+
+})
---
+++ detail-album5.js
@@ -0,0 +1,55 @@
+window.addEventListener('load',function(){
+
+    /*Funcion Para menu desplegable responsive*/
+    const navToggle = document.querySelector(".nav-toggle")
+    const navMenu =document.querySelector(".nav-menu")
+    
+    navToggle.addEventListener("click",() =>{
+        navMenu.classList.toggle("nav-menu_visible");
+    })
+    
+
+   const proxy = `https://cors-anywhere.herokuapp.com/`;
+
+let apiAlbums = `${proxy}https://api.deezer.com/album/12114240`;
+
+fetch (apiAlbums)                                           
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+    console.log(datos)
+   
+    fotoAlb5= document.querySelector(".fotoartist5");
+    nombreAlb5 = document.querySelector(".nombrealbum5");
+    nombreArt5 = document.querySelector(".nombreart5");
+    nombreGen5 = document.querySelector(".nombregen5");
+    fechaAlb5 = document.querySelector(".fechaalb5");
+    listaTemas5 = document.querySelector(".temasalbum5")
+
+  fotoAlb5.innerHTML +=`
+    <img src="${datos.cover_medium}">`;
+    
+    nombreAlb5.innerHTML +=`<h2>${datos.title}</h2>`;
+    nombreArt5.innerHTML +=`<h2>${datos.artist.name}</h2>`;
+    nombreGen5.innerHTML += `<h2>${datos.genres.data[0].name}</h2>`;
+    fechaAlb5.innerHTML += `<h2>${datos.release_date}</h2>`;
+    listaTemas5.innerHTML += `<li>${datos.tracks.data[0].title}</li>
+    <li>${datos.tracks.data[1].title}</li>
+    <li>${datos.tracks.data[2].title}</li>
+    <li>${datos.tracks.data[3].title}</li>
+    <li>${datos.tracks.data[4].title}</li>
+    <li>${datos.tracks.data[5].title}</li>
+    <li>${datos.tracks.data[6].title}</li>
+    <li>${datos.tracks.data[7].title}</li>
+    <li>${datos.tracks.data[8].title}</li>
+    <li>${datos.tracks.data[9].title}</li>`;
+    
+    
+
+    })
+    .catch(function(error){
+console.log(error)
+    })
+
+})
